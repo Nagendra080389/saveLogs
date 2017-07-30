360d18f4599f42a1e628a617183a4e5098e750bb
US\nagesingh <nagesingh@deloitte.com>
2017-07-30 13:31:24 +0530

    Changed hudson plugin version

--- src/main/java/hudson/plugins/saveLogs/PMDStructure.java
+++ src/main/java/hudson/plugins/saveLogs/PMDStructure.java
@@ -1,14 +1,9 @@
 package hudson.plugins.saveLogs;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
-@Document(collection = "SalesForceClass")
 public class PMDStructure implements Serializable{
-
-    @Id
     private String id;
     private String fileName;
     private String salesforceID;
